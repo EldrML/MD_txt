ec6060702fa2fb07a5084633591e5251ce3013ea
Liam Durbin <durbinl@purdue.edu>
2019-07-24 22:01:51 -0400

    Further Progress
    Today I added a few new things to the game to make it easier to code in the coming days. Instead of hardcoding some things, we are now using C# to generate Serialized Fields. Wow that's handy.

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+	[SerializeField] Text textComponent; //SerializeField: Makes the variable visible within the inspector.
+	[SerializeField] State startingState;
+
+	State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		state = startingState;
+		textComponent.text = state.GetStateStory(); //Specifies the text field within the tex component. Allows us to call the method State in this script.
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")] //This results in a "state" asset appearing at the top of the context menu, it lets you select it repeatedly.
+public class State : ScriptableObject
+{
+	[TextArea(20,20)] [SerializeField] string storyText; //TextArea numbers: First, minimum size of the field. Second, amount of lines before scrolling.
+
+	public string GetStateStory()
+	{
+		return storyText;
+	}
+}
